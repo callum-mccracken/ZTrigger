00f66df60ccf2040aeaca9862b608464e2ed5446
Callum McCracken <callum189@gmail.com>
2022-11-15 21:25:09 -0800

    oops needed to remove newline

--- filter.py
+++ filter.py
@@ -9,19 +9,6 @@ LIST_DISK_OUTPUT_DIR = os.path.join(MTPPP_ROOT, "run")
 LIST_DISK_OUTPUT_FILE = os.path.join(
     LIST_DISK_OUTPUT_DIR, "CA-SFU-T2_LOCALGROUPDISK_2022-11-13_v66.3.0.txt")
 
-def readcmd(cmd):
-    """Get the output of a terminal command."""
-    ftmp = tempfile.NamedTemporaryFile(
-        suffix='.out', prefix='tmp', delete=False)
-    fpath = ftmp.name
-    ftmp.close()
-    os.system(cmd + " > " + fpath)
-    data = ""
-    with open(fpath, 'r') as tmpfile:
-        data = tmpfile.read()
-        tmpfile.close()
-    os.remove(fpath)
-    return data
 
 def main():
     """Filter to only get the files we care about for SFs"""
@@ -34,15 +21,17 @@ def main():
     filtered_lines = []
     for line in lines:
         if "Main" in line or ("Zmumu" in line and "Powheg" in line):
-            print("Checking", line)
+            line = line.replace('\n', '')
             filtered_lines.append(line)
             cmd = "rucio list-datasets-rse " + RSE + " | grep " + line
-            cmd_output = readcmd(cmd)
-            print(cmd_output)
+            fpath = "temp.out"
+            os.system(cmd + " > " + fpath)
+            with open(fpath, 'r') as tmpfile:
+                cmd_output = tmpfile.read()
             if line in str(cmd_output):
                 print("Adding", line)
             else:
-                raise ValueError("Files do not exist!")
+                raise ValueError("File" + line + "does not exist!")
 
     out_path = os.path.join(LIST_DISK_OUTPUT_DIR, "filtered.txt")
     with open(out_path, "w+") as filtered_file:
