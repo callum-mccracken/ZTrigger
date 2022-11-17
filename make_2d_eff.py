a23642a612bbb117b83521cba22c5f671a75e47f
Callum McCracken <callum189@gmail.com>
2022-11-16 21:56:42 -0800

    update wtph naming format

--- make_2d_eff.py
+++ make_2d_eff.py
@@ -195,7 +195,7 @@ def main():
         root_files[d_mc][year] = {}
         root_files[d_mc][year][period] = {}
         for var in c.VARIATIONS:
-            var_conf_name = d_mc+str(year)+"_".join(
+            var_conf_name = d_mc+str(year)+"_"+"_".join(
                 [period, var, version, trigger_type])+".root"
             var_conf = os.path.join(input_dir, var_conf_name)
             if not os.path.exists(var_conf):
